db63a03fb67b4775df4f1f1584b6379a907fc3f6
Ezee Peezy <vladlibero@gmail.com>
2019-11-05 00:17:59 +0200

    express, pony all set

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+
+app.get('/pony', (req, res) => {
+  res.sendFile(`${__dirname}/public/pony.html`)
+})
+
+app.listen(4500, '0.0.0.0', () => {
+    console.log("Express pony server started at http://0.0.0.0:4500")
+})
